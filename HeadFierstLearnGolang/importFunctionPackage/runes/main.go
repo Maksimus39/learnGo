ffb9f5075ee297e23c825f0c348f00c68960cdde
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-03-29 15:12:53 -0700

    руны

---
+++ HeadFierstLearnGolang/importFunctionPackage/runes/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println('A')
+	fmt.Println('X')
+	fmt.Println('W')
+	fmt.Println('q')
+	fmt.Println('Q')
+	fmt.Println('L')
+	fmt.Println('U')
+}
