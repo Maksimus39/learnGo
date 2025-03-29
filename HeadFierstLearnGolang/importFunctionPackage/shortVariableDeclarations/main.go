2d591a4306c1d5b502f50483921656223051f375
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-03-29 15:36:22 -0700

    короткое объявление   переменной

---
+++ HeadFierstLearnGolang/importFunctionPackage/shortVariableDeclarations/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	quantity := 4
+	length, width := 1.2, 2.4
+	customerName := "gopher"
+
+	fmt.Println(quantity)
+	fmt.Println(length, width)
+	fmt.Println(customerName)
+}
